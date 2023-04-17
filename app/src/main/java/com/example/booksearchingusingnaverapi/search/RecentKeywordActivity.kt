9922d1ed3dcb5fefb831f9f0982b965720af1fca
김도현 <kohyun821@naver.com>
2023-04-18 02:20:12 +0900

    최근 검색어 화면 완료

---
+++ app/src/main/java/com/example/booksearchingusingnaverapi/search/RecentKeywordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booksearchingusingnaverapi.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booksearchingusingnaverapi.R
+
+class RecentKeywordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recent_keyword)
+    }
+}
